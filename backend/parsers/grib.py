6aafe7cc8bc568139c9830199b9d3c26d1abf049
imtherealF1 <vasilmichas@gmail.com>
2026-04-13 20:25:43 +0300

    revert back to original structure

--- backend/parsers/grib.py
+++
@@ -1,7 +0,0 @@
-import xarray as xr
-
-def get_temperature():
-    ds = xr.open_dataset("data/gfs.grib2", engine="cfgrib")
-    temp_k = ds['t2m'].values
-    temp_c = temp_k - 273.15
-    return float(temp_c.mean())
